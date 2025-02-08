7205e486edc455bddb80b0568c70ec49fa4e3e09
Lachezar Boyadzhiev <l.boyadzhievv@gmail.com>
2025-02-08 12:32:24 +0200

    Build Custom Form Component

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,25 @@ import './App.css';
 
 // components
 import Input from './components/Input';
-// import Button from './components/Button';
-// import Container from './components/Container';
-import { useRef } from 'react';
+import Button from './components/Button';
+import Form from './components/Form';
 
 function App() {
-  const inputRef = useRef<HTMLInputElement>(null);
+  function handleSave(data: unknown) {
+    const extractedData = data as { name: string; age: string };
+
+    console.log(extractedData);
+  }
+
   return (
     <main>
-      <Input label='Test' id='test' ref={inputRef} />
+      <Form onSave={handleSave}>
+        <Input type='text' label='Name' id='name' />
+        <Input type='number' label='Age' id='age' />
+        <p>
+          <Button>Save</Button>
+        </p>
+      </Form>
     </main>
   );
 }
